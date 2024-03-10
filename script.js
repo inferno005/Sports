5c53b60c644fbdcc4ae0650a8e836e2c1f5a5abd
Inferno <mj.manojoshi005@gmail.com>
2024-03-10 23:30:19 +0530

    css

--- script.js
+++ script.js
@@ -13,27 +13,26 @@ gsap.to("#nav",{
     }
 })
 gsap.to("#main",{
-    backgroundColor : "#fff",
+    backgroundColor : "rgba(0, 0, 0, 0.900)",
     duration: "0.5",
     scrollTrigger:{
         trigger:"#page2",
         scroller:"body",
-        markers:"true",
+        // markers:"true",
         start:"top, 80%",
         end:"top, 50%",
         scrub:2,
     }
 })
-
-gsap.to("#effect",{
-    color:"#000",
-    duration:0.5,
-    scrollTrigger:{
-        // markers:true,
-        start:"top -30%",
-        end:"top -80%",
-        scrub:2,
-    }
-})
+// gsap.to("#effect",{
+//     color:"#fff",
+//     duration:0.5,
+//     scrollTrigger:{
+//         // markers:true,
+//         start:"top -30%",
+//         end:"top -80%",
+//         scrub:2,
+//     }
+// })
 
 
